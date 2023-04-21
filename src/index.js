5fc7f0ab5a3a76795513c2e58762b2bb9d8e563e
Kyle Crawford <kcraw217@gmail.com>
2023-04-21 10:02:51 -0700

    get currency api data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyService from './services/currency-service.js';
+
+// Business Logic
+
+function getCurrencies() {
+  CurrencyService.getCurrencies()
+    .then(function (currencyResponse) {
+      if (currencyResponse instanceof Error) {
+        const errorMessage = `There was a problem accessing the currency data from ExchangeRate API:
+        ${currencyResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      const currencies = currencyResponse.conversion_rates;
+      printCurrencies(currencies);
+    })
+    .catch(function (error) {
+      printError(error);
+    });
+}
+
+
+
+// UI Logic
+
+function printCurrencies(response) {
+  console.log(response);
+}
+
+function printError(error) {
+  console.log(error);
+}
+
+
+window.addEventListener("load", function () {
+  getCurrencies();
+});
--- src/services/currency-service.js
+++ src/services/currency-service.js
@@ -1,16 +1,16 @@
 export default class CurrencyService {
-  static async getCurrencies() {
-    try {
-      const repsonse = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
-      const jsonifiedResponse = await response.json();
-      if (!response.ok) {
-        const errorMessage = `${response.status} ${response.statusText}
-        ${jsonifiedResponse}`;
-        throw new Error(errorMessage);
-      }
-      return jsonifiedResponse;
-    } catch (error) {
-      return error;
-    }
+  static getCurrencies() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(function (response) {
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }
+      })
+      .catch(function (error) {
+        return error;
+      });
   }
 }
